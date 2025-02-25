0b657841a3af3e4b5bf73acabcf9f6cde3811b13
Dattu514 <kamineni.dattatteya@hcltech.com>
2025-02-25 17:57:19 +0530

    Add new prod

--- Practice1/script.js
+++ Practice1/script.js
@@ -1,2 +1,22 @@
 console.log("Hello World");
 console.log("Hello World1");
+
+//write a armstrong number code
+
+let num = 153;   
+let sum = 0;
+let temp = num;  
+
+while (num > 0) {
+    let rem = num % 10;
+    sum += rem * rem * rem;
+    num = parseInt(num / 10);
+}
+
+if (temp == sum) {           
+    console.log(temp + " is an Armstrong number");
+} else {
+    console.log(temp + " is not an Armstrong number");
+}            
+
+
